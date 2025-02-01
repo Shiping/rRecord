a5ddaff6e53a8f39af546aeec18b799cbb3319c7
Shiping <shipinglau@gmail.com>
2025-02-01 18:04:06 +0800

    Initial Commit

---
+++ RecordH/RecordHApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordHApp.swift
+//  RecordH
+//
+//  Created by liushiping on 2025/2/1.
+//
+
+import SwiftUI
+
+@main
+struct RecordHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecordHTests/RecordHTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecordHTests.swift
+//  RecordHTests
+//
+//  Created by liushiping on 2025/2/1.
+//
+
+import Testing
+@testable import RecordH
+
+struct RecordHTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
